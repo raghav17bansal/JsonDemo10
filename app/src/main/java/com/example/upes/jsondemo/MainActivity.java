1e0d9d05c719c3bf22b399d40575b18cba192241
Raghav Bansal <f20170196@pilani.bits-pilani.ac.in>
2017-12-21 13:08:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/upes/jsondemo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.upes.jsondemo;
+
+import java.util.List;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity implements FetchDataListener{
+    private ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        // show progress dialog
+        dialog = ProgressDialog.show(this, "", "Loading...");
+
+        String url = "http://10.0.2.2/test/apps.php";
+        FetchDataTask task = new FetchDataTask(this);
+        task.execute(url);
+    }
+
+    @Override
+    public void onFetchComplete(List<Application> data) {
+        // dismiss the progress dialog
+        if(dialog != null)  dialog.dismiss();
+        // create new adapter
+        ApplicationAdapter adapter = new ApplicationAdapter(this, data);
+        // set the adapter to list
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onFetchFailure(String msg) {
+        // dismiss the progress dialog
+        if(dialog != null)  dialog.dismiss();
+        // show failure message
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+}
